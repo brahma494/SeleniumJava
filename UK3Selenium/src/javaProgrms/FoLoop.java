c325822f286a4908997ca102b534766bc294625a
brahma494 <seleniumauto111@gmail.com>
2017-03-17 17:13:41 +0530

    Update FoLoop.java

--- UK3Selenium/src/javaProgrms/FoLoop.java
+++ UK3Selenium/src/javaProgrms/FoLoop.java
@@ -5,12 +5,12 @@ public class FoLoop {
 	public static void main(String[] args) {
 		
 		// Print 1-10 Numbers
-		for(int i=1;i<=10;i++)
+		for(int i=1;i<=12;i++)
 		{
 			System.out.println(i);
 		}
 		
-	//	o/p:12345678910
+	//	o/p:123456789101112
 		//Print numbers
 	
 	}
